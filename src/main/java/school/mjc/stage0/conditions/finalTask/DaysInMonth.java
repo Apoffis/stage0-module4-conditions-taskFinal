45c378347af85e5f6b7f93307f01c02d53a823ee
Apoffis <abc22_08@mail.ru>
2022-10-22 19:37:35 +0400

    conditions task final commit

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -3,5 +3,25 @@ package school.mjc.stage0.conditions.finalTask;
 public class DaysInMonth {
     public void printDays(int year, int month) {
 
+        boolean leap = false;
+
+        if(year%400==0)
+            leap = true;
+        else  if(year%100!=0)
+        {
+            leap = year % 4 == 0;
+        }
+        else
+            leap = false;
+
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> System.out.println(31);
+            case 2 -> {
+                System.out.println(leap ? 29 : 28);
+            }
+            case 4, 6, 9, 11 -> System.out.println(30);
+            default -> System.out.println("invalid date");
+        }
+
     }
 }
